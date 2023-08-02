571215d6999ed6a536732010114b390e1bc510c5
Thiago Madureira Braga <thiago@thau.tech>
2023-08-02 12:17:17 +0200

    feat(component)Ç implement education

--- components/Education.vue
+++ components/Education.vue
@@ -1,7 +1,49 @@
 <template>
-    <section class="container mx-auto px-4 py-20">
-        EDUCATION
+    <section class="container px-4 pb-4 mx-auto">
+        <div class="card p-4">
+            <h3 class="text-xl text-slate-700 mb-2">EDUCATION</h3>
+            <div class="education">
+                <div>
+                    <h4 class="course">{{ $t("component.education.ufrj.course") }}</h4>
+                    <h4 class="duration">{{ $t("component.education.ufrj.duration") }}</h4>
+                    <h4 class="school">{{ $t("component.education.ufrj.school") }}</h4>
+                </div>
+                <p class="summary">{{ $t("component.education.ufrj.summary") }}</p>
+            </div>
+            <div class="education">
+                <div>
+                    <h4 class="course">{{ $t("component.education.ufmg.course") }}</h4>
+                    <h4 class="duration">{{ $t("component.education.ufmg.duration") }}</h4>
+                    <h4 class="school">{{ $t("component.education.ufmg.school") }}</h4>
+                </div>
+                <p class="summary">{{ $t("component.education.ufmg.summary") }}</p>
+            </div>
+        </div>
     </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+div.education {
+    --at-apply: mb-4
+}
+
+div.education>div {
+    --at-apply: flex flex-col lg:flex-row text-base md:text-xl mb-2
+}
+
+h4.course {
+    --at-apply: text-thau font-bold
+}
+
+h4.duration {
+    --at-apply: text-slate-700 font-light ml-0 lg:ml-4 order-first lg:order-none
+}
+
+h4.school {
+    --at-apply: text-slate-700 ml-0 lg:ml-auto
+}
+
+p.summary {
+    --at-apply: text-slate-500 text-justify text-sm md:text-base
+}
+</style>
--- components/Experience.vue
+++ components/Experience.vue
@@ -2,64 +2,64 @@
     <section class="container px-4 pb-4 mx-auto">
         <div class="card p-4">
             <h3 class="text-xl text-slate-700 mb-2">EXPERIENCE</h3>
-            <div class="position">
+            <div class="experience">
                 <div>
-                    <h4 class="role">{{ $t("component.experience.thau.role") }}</h4>
-                    <h4 class="date">{{ $t("component.experience.thau.date") }}</h4>
-                    <h4 class="site">{{ $t("component.experience.thau.site") }}</h4>
+                    <h4 class="position">{{ $t("component.experience.thau.position") }}</h4>
+                    <h4 class="duration">{{ $t("component.experience.thau.duration") }}</h4>
+                    <h4 class="company">{{ $t("component.experience.thau.company") }}</h4>
                 </div>
-                <p class="work">{{ $t("component.experience.thau.work") }}</p>
+                <p class="summary">{{ $t("component.experience.thau.summary") }}</p>
             </div>
-            <div class="position">
+            <div class="experience">
                 <div>
-                    <h4 class="role">{{ $t("component.experience.una.role") }}</h4>
-                    <h4 class="date">{{ $t("component.experience.una.date") }}</h4>
-                    <h4 class="site">{{ $t("component.experience.una.site") }}</h4>
+                    <h4 class="position">{{ $t("component.experience.una.position") }}</h4>
+                    <h4 class="duration">{{ $t("component.experience.una.duration") }}</h4>
+                    <h4 class="company">{{ $t("component.experience.una.company") }}</h4>
                 </div>
-                <p class="work">{{ $t("component.experience.una.work") }}</p>
+                <p class="summary">{{ $t("component.experience.una.summary") }}</p>
             </div>
-            <div class="position">
+            <div class="experience">
                 <div>
-                    <h4 class="role">{{ $t("component.experience.linksaude.role") }}</h4>
-                    <h4 class="date">{{ $t("component.experience.linksaude.date") }}</h4>
-                    <h4 class="site">{{ $t("component.experience.linksaude.site") }}</h4>
+                    <h4 class="position">{{ $t("component.experience.linksaude.position") }}</h4>
+                    <h4 class="duration">{{ $t("component.experience.linksaude.duration") }}</h4>
+                    <h4 class="company">{{ $t("component.experience.linksaude.company") }}</h4>
                 </div>
-                <p class="work">{{ $t("component.experience.linksaude.work") }}</p>
+                <p class="summary">{{ $t("component.experience.linksaude.summary") }}</p>
             </div>
-            <div class="position">
+            <div class="experience">
                 <div>
-                    <h4 class="role">{{ $t("component.experience.ats.role") }}</h4>
-                    <h4 class="date">{{ $t("component.experience.ats.date") }}</h4>
-                    <h4 class="site">{{ $t("component.experience.ats.site") }}</h4>
+                    <h4 class="position">{{ $t("component.experience.ats.position") }}</h4>
+                    <h4 class="duration">{{ $t("component.experience.ats.duration") }}</h4>
+                    <h4 class="company">{{ $t("component.experience.ats.company") }}</h4>
                 </div>
-                <p class="work">{{ $t("component.experience.ats.work") }}</p>
+                <p class="summary">{{ $t("component.experience.ats.summary") }}</p>
             </div>
         </div>
     </section>
 </template>
 
 <style scoped>
-div.position {
+div.experience {
     --at-apply: mb-4
 }
 
-div.position>div {
+div.experience>div {
     --at-apply: flex flex-col lg:flex-row text-base md:text-xl mb-2
 }
 
-h4.role {
+h4.position {
     --at-apply: text-thau font-bold
 }
 
-h4.date {
+h4.duration {
     --at-apply: text-slate-700 font-light ml-0 lg:ml-4 order-first lg:order-none
 }
 
-h4.site {
+h4.company {
     --at-apply: text-slate-700 ml-0 lg:ml-auto
 }
 
-p.work {
+p.summary {
     --at-apply: text-slate-500 text-justify text-sm md:text-base
 }
 </style>
