a04a7783e87e10ca3d144452d6509e81f57a1ca7
Thiago Madureira Braga <thiago@thau.tech>
2023-09-06 19:11:55 +0200

    feat(color): change mode switcher to button

--- components/global/ColorMode.vue
+++ components/global/ColorMode.vue
@@ -1,17 +1,21 @@
 <template>
-<select v-model="$colorMode.preference">
-    <option value="system">System</option>
-    <option value="light">Light</option>
-    <option value="dark">Dark</option>
-</select>
+
+<button @click="toggle">
+    <!-- material-symbols:light-mode-outline-sharp -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" v-if="$colorMode.preference != 'dark'">
+        <path :fill="currentColor" d="M12 15q1.25 0 2.125-.875T15 12q0-1.25-.875-2.125T12 9q-1.25 0-2.125.875T9 12q0 1.25.875 2.125T12 15Zm0 2q-2.075 0-3.538-1.463T7 12q0-2.075 1.463-3.538T12 7q2.075 0 3.538 1.463T17 12q0 2.075-1.463 3.538T12 17ZM1 13v-2h4v2H1Zm18 0v-2h4v2h-4Zm-8-8V1h2v4h-2Zm0 18v-4h2v4h-2ZM6.35 7.75L3.875 5.275l1.4-1.4L7.75 6.35l-1.4 1.4Zm12.375 12.375L16.25 17.65l1.4-1.4l2.475 2.475l-1.4 1.4ZM17.65 7.75l-1.4-1.4l2.475-2.475l1.4 1.4L17.65 7.75ZM5.275 20.125l-1.4-1.4L6.35 16.25l1.4 1.4l-2.475 2.475ZM12 12Z"/>
+    </svg>
+    <!-- material-symbols:dark-mode-outline -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" v-if="$colorMode.preference == 'dark'">
+        <path :fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12q0-3.75 2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21Zm0-2q2.2 0 3.95-1.213t2.55-3.162q-.5.125-1 .2t-1 .075q-3.075 0-5.238-2.163T9.1 7.5q0-.5.075-1t.2-1q-1.95.8-3.163 2.55T5 12q0 2.9 2.05 4.95T12 19Zm-.25-6.75Z"/>
+    </svg>
+</button>
 </template>
 
-<style scoped>
-select {
-    --at-apply: bg-transparent text-lg text-slate-300 hover:text-slate-100
-}
-</style>
-
 <script setup lang="ts">
-
+const currentColor = "white";
+const colorMode = useColorMode();
+const toggle = () => {
+    colorMode.preference = (colorMode.preference == "dark") ? "light" : "dark"
+};
 </script>
